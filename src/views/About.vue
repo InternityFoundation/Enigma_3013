eb6cdf587d118008898f2e983555839d2b946695
nehasangeetajha <43818333+nehasangeetajha@users.noreply.github.com>
2020-07-04 15:42:51 +0530

    Home Page Modified

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section id="about">
+	<div class="container"> 
+	<div class="row">
+	<div class="col-md-6"> 
+		<h2>Putting Personality First</h2>
+		<div class="about-content">
+		Time is the most precious thing that we have.Don't Waste yours carrier that is not for you.	
+		</div>  
+		 <button type="button" class="btn btn-primary">Read More>>
+		 </button>
+		</div>
+		 <div class="col-md-6">
+		   <p>Explore</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:80%;">80%</div>
+		</div>
+
+		   <p>Understand</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:85%;">85%</div>
+		</div>
+		   <p>Develop</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:90%;">90%</div>
+		</div>
+		<p>Prepare</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:100%;">100%</div>
+		</div>
+		<p>Personality</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:90%;">90%</div>
+		</div>
+	</div>
+    </div>
+    </div>     
+
+</section>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-md-4">
+      
+    </div>
+      <div class="col-md-8">
+        <img src="https://image.freepik.com/free-vector/business-leader-consulting-hr-expert_1262-21207.jpg" alt="">
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
