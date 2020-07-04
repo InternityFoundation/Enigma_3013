8c095db49d558257a33ec9df5aa25f6b02df3943
nehasangeetajha <43818333+nehasangeetajha@users.noreply.github.com>
2020-07-04 22:33:22 +0530

    UI updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,652 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="home">
+
+<!--slider-->
+<div class="row mt-5">  
+ <div class="col-md-12">
+  <svg id="a17dd300-7621-4978-a344-c06233c14e82" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="887.87606" height="652.02406" viewBox="0 0 887.87606 652.02406"><title>success_factors</title><path d="M1043.93777,757.41073c-2.84541,25.87621-152.22584,23.704-151.99948-.00154C894.78367,731.53451,1044.1641,733.70675,1043.93777,757.41073Z" transform="translate(-156.06197 -123.98797)" fill="#e6e6e6"/><polygon points="785.259 567.02 779.204 580.14 768.102 579.131 761.038 555.918 772.139 549.863 785.259 567.02" fill="#ffb8b8"/><polygon points="823.372 597.237 823.611 606.38 806.454 607.39 806.454 596.288 823.372 597.237" fill="#ffb8b8"/><polygon points="817.555 430.772 813.518 511.511 823.611 599.316 793.333 603.353 774.158 492.336 768.102 416.642 817.555 430.772" fill="#2f2e41"/><path d="M1021.05174,525.49168s12.11093,79.7303-13.12018,105.97066-59.54542,72.66559-59.54542,72.66559L924.16428,673.8506l61.5639-65.60088L973.61725,563.843l-49.453-23.21262,8.074-55.50844,72.6656-1.00924Z" transform="translate(-156.06197 -123.98797)" fill="#2f2e41"/><circle cx="805.44435" cy="194.60862" r="22.20338" fill="#ffb8b8"/><path d="M980.682,323.64281s1.00925,31.28657,6.05547,35.32355-18.1664,21.19413-18.1664,21.19413l-15.13867-24.22186V333.73525Z" transform="translate(-156.06197 -123.98797)" fill="#ffb8b8"/><polygon points="814.528 242.043 826.781 223.541 833.703 232.96 843.796 261.219 835.722 371.226 788.287 372.236 782.232 246.08 797.37 227.914 814.528 242.043" fill="#d0cde1"/><path d="M919.11806,373.09578l-8.074-1.00924s-2.01849,1.00924-3.02773,8.07395-13.12018,69.63786-13.12018,69.63786l16.14791,76.70257,18.1664-24.22186L918.10881,459.8908l11.10169-42.38827Z" transform="translate(-156.06197 -123.98797)" fill="#2f2e41"/><polygon points="865.999 248.099 872.054 248.099 887.193 329.847 873.064 393.43 858.934 370.217 862.971 344.986 860.953 322.783 853.888 309.662 865.999 248.099" fill="#2f2e41"/><path d="M932.23823,700.091s6.05547-2.01849,6.05547,2.01849-4.037,16.14791-4.037,16.14791,6.05547,37.342-3.02773,35.32355-16.14791-19.17564-17.15716-24.22186-6.05546-31.28658-6.05546-31.28658-8.074-19.17564-3.02773-20.18488,19.17564-5.04622,19.17564-5.04622S916.09032,698.07246,932.23823,700.091Z" transform="translate(-156.06197 -123.98797)" fill="#2f2e41"/><path d="M961.50632,731.37753s16.14791-10.09245,19.17564-2.01849a84.80084,84.80084,0,0,1,4.037,18.1664s4.037,13.12017-12.11093,11.10169-14.12942-2.01849-15.13867-8.074S961.50632,731.37753,961.50632,731.37753Z" transform="translate(-156.06197 -123.98797)" fill="#2f2e41"/><path d="M974.6265,318.59659l-4.037-1.00925s-3.02773-17.15715-10.09244-15.13866-25.23111,4.037-25.23111-4.037,17.15715-15.13867,27.24959-14.12942,27.2496,4.037,26.24036,19.17564-7.52165,29.86536-7.52165,29.86536l-.52605-8.97562Z" transform="translate(-156.06197 -123.98797)" fill="#2f2e41"/><polygon points="797.37 254.154 814.528 242.043 797.37 227.914 791.414 235.062 797.37 254.154" fill="#d0cde1"/><polygon points="829.666 253.145 814.528 242.043 826.638 223.877 833.703 232.96 829.666 253.145" fill="#d0cde1"/><path d="M912.05335,372.08654l32.29582-13.12018,7.56933-5.55084,22.708,100.41981,15.13866-45.416-4.5416-58.03155,41.88364,21.69876-14.12942,68.62861-2.01849,26.24036,6.05547,21.19413s21.19413,15.13866,14.12942,31.28657-15.13867,17.15716-15.13867,17.15716-34.3143-32.29582-36.33279-40.36978-5.04622-22.20337-5.04622-22.20337-17.15716,64.59164-37.342,63.58239-20.18488-22.20337-20.18488-22.20337l5.04622-22.20338,8.07395-23.21262-4.037-38.35129Z" transform="translate(-156.06197 -123.98797)" fill="#2f2e41"/><rect x="0.30042" y="0.39886" width="703.57565" height="450.60114" fill="#e6e6e6"/><rect x="20.419" y="56.91548" width="663.33848" height="171.77293" fill="#fff"/><rect x="185.4182" y="81.72713" width="140.28123" height="8.05267" fill="#e6e6e6"/><rect x="185.4182" y="111.10108" width="216.62475" height="8.05267" fill="#6c63ff"/><rect x="185.4182" y="138.77561" width="176.5444" height="8.05267" fill="#e6e6e6"/><rect x="185.4182" y="166.34556" width="103.06376" height="8.05267" fill="#e6e6e6"/><rect x="185.4182" y="193.9155" width="155.54993" height="8.05267" fill="#e6e6e6"/><rect x="121.4805" y="78.86424" width="17.17729" height="17.17729" fill="#e6e6e6"/><rect x="121.4805" y="106.53877" width="17.17729" height="17.17729" fill="#6c63ff"/><rect x="121.4805" y="134.2133" width="17.17729" height="17.17729" fill="#e6e6e6"/><rect x="121.4805" y="161.88783" width="17.17729" height="17.17729" fill="#e6e6e6"/><rect x="121.4805" y="189.56235" width="17.17729" height="17.17729" fill="#e6e6e6"/><rect x="533.73553" y="117.9903" width="57.25764" height="57.25764" fill="#e6e6e6"/><rect x="20.419" y="252.54576" width="663.33848" height="171.77293" fill="#fff"/><rect x="151.06361" y="267.81206" width="17.17729" height="17.17729" fill="#e6e6e6"/><rect x="121.4805" y="267.81206" width="17.17729" height="17.17729" fill="#6c63ff"/><path d="M568.78582,429.36336a65.04552,65.04552,0,0,0-105.003-9.69993l-4.18616-3.65794a70.59376,70.59376,0,0,1,113.973,10.5262Z" transform="translate(-156.06197 -123.98797)" fill="#e6e6e6"/><path d="M571.40549,501.78181l-4.6134-3.102a65.07767,65.07767,0,0,0,1.99373-69.31643l4.78387-2.83167a70.63741,70.63741,0,0,1-2.1642,75.25012Z" transform="translate(-156.06197 -123.98797)" fill="#6c63ff"/><path d="M460.17592,509.52655a70.58952,70.58952,0,0,1-.57926-93.52106l4.18616,3.65794a65.03084,65.03084,0,0,0,.53365,86.15416Z" transform="translate(-156.06197 -123.98797)" fill="#6c63ff"/><path d="M512.776,533.04037a70.70886,70.70886,0,0,1-52.6-23.51382l4.14055-3.709a65.04336,65.04336,0,0,0,102.47562-7.1378l4.6134,3.102A70.5538,70.5538,0,0,1,512.776,533.04037Z" transform="translate(-156.06197 -123.98797)" fill="#6c63ff"/><rect x="533.73553" y="305.03194" width="57.25764" height="57.25764" fill="#e6e6e6"/><rect x="119.09476" y="342.2494" width="57.25764" height="57.25764" fill="#e6e6e6"/><rect width="703.57565" height="29.89047" fill="#6c63ff"/><circle cx="22.21219" cy="15.28159" r="5.53997" fill="#fff"/><circle cx="43.24053" cy="15.28159" r="5.53997" fill="#fff"/><circle cx="64.26886" cy="15.28159" r="5.53997" fill="#fff"/></svg>
+ </div>
+ </div> 
+<!---about-->
+<section id="about">
+	<div class="container"> 
+	<div class="row">
+	<div class="col-md-6"> 
+		<h2>Putting Personality First</h2>
+		<div class="about-content">
+		Time is the most precious thing that we have.Don't Waste yours carrier that is not for you.Developing a good work ethic is key. Apply yourself at whatever you do, whether you're a janitor or taking your first summer job because that work ethic will be reflected in everything you do in life	
+		</div>  
+		<button onclick="location.href= 'form.html';" type = "submit" value = "Create your profile for free" class = "btn btn-success btn-block">Create your profile for free
+		</button>
+		</div>
+		 <div class="col-md-6">
+		   <p>Explore</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:80%;">80%</div>
+		</div>
+
+		   <p>Understand</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:85%;">85%</div>
+		</div>
+		   <p>Develop</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:90%;">90%</div>
+		</div>
+		<p>Prepare</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:100%;">100%</div>
+		</div>
+		<p>Personality</p>
+		   <div class="progress">
+		   <div class="progress-bar" style="width:90%;">90%</div>
+		</div>
+	</div>
+    </div>
+    </div>     
+
+</section>
+<!---->
+<section id="services">
+	<div class="container">
+		<h1>Our Services</h1>
+		<div class="row services"> 
+			<div class="col-md-3 text-center">
+				<div class="icon">
+					<i class="fa fa-desktop"></i>
+				</div>
+				<h3>Hospitality</h3>
+				<p> We are helping many companies to Recruit Right Talent from more than 22 Years and placed more than 10000 Candidates in Various Companies </p>
+					
+				</div>
+				<div class="col-md-3 text-center">
+				<div class="icon">
+					<i class="fa fa-tablet"></i>
+				</div>
+				<h3>Experience</h3>
+                                <p> Better understanding of job market, and an equally good understanding of yourself & where you stand, a kind of SWOT analysis.</p>
+
+
+
+					
+				</div>
+				<div class="col-md-3 text-center">
+				<div class="icon">
+					<i class="fa fa-line-chart"></i>
+				</div>
+				<h3>Growth</h3>
+                                <p>  Not hard work, but smart work, so as to land yourself in a good job and company.</p>
+				
+				</div>
+				<div class="col-md-3 text-center">
+				<div class="icon">
+					<i class="fa fa-paint-brush"></i>
+				</div>
+				<h3>Jobs</h3>
+				<p>  We wish employers must enjoy their job.</p>
+
+					
+				</div>
+	</div>
+</div>
+
+
+</section>
+
+<!--team-->
+<section id="team">
+<div class="container">
+	<h1>OUR TEAM</h1>
+	<div class="row">
+		<div class="col-md-3 profile-pic text-center">
+				<div class="img-box">
+					
+					<ul>
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-whatsapp"></i></li></a>
+					<a href="#"><li><i class="fa fa-instagram"></i></li></a>
+					</ul>
+				</div>
+				<h2>Siddhant Gupta</h2>
+				<p>like and ask question in comment.
+				</p>
+			</div>
+				<div class="col-md-3 profile-pic text-center">
+				<div class="img-box">
+					
+					<ul>
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-whatsapp"></i></li></a>
+					<a href="#"><li><i class="fa fa-instagram"></i></li></a>
+					</ul>
+				</div>
+				<h2>Sonali</h2>
+			
+				<p>like and ask question in comment.
+				</p>
+			</div>
+				<div class="col-md-3 profile-pic text-center">
+				<div class="img-box">
+					
+					<ul>
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-whatsapp"></i></li></a>
+					<a href="#"><li><i class="fa fa-instagram"></i></li></a>
+					</ul>
+				</div>
+				<h2>Harshit Garg</h2>
+			
+				<p>like and ask question in comment.
+				</p>
+			</div>
+				<div class="col-md-3 profile-pic text-center">
+				<div class="img-box">
+					
+					<ul>
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-linkedIn"></i></li></a>
+					<a href="#"><li><i class="fa fa-instagram"></i></li></a>
+					</ul>
+				    </div>
+				         <h2>Sangeeta Jha</h2>
+				        
+				         <p>like and ask question in comment.
+				         </p>
+			</div>
+		    </div>
+	</div>
+</section>
+
+
+
+
+<section id="promo">
+<div class="container">
+	<p>Get free passes and Goodies and exciting hampers</p>
+        
+	<a href="#" class="btn btn->primary">CONTACT US- 5433245</a>
+</div>
+</section>
+
+
+
+
+
+
+
+
+
+<!--getintouch-->
+<section id="contact">
+<div class="container">
+	<h1>Get In Touch</h1>
+	<div class="row">
+	<div class="col-md-6">
+	<form class="contact-form">
+	<div class="form-group">
+	<input type="text" class="form-control" placeholder ="Your Name">
+	</div>
+	<div class="form-group">
+	<input type="number" class="form-control" placeholder ="Phone Number">
+	</div>
+	<div class="form-group">
+	<input type="E-Mail" class="form-control" placeholder ="Email Id">
+	</div>
+	<div class="form-group">
+	<textarea class="form-control" rows="4" placeholder ="Your Message"></textarea>
+	</div>
+	<button type="submit" class="btn btn-primary">SEND MESSAGE</button>
+	</form>
+	</div>
+	<div class="col-md-6 contact-info">
+	<div class="follow"><b>Address:</b><i class="fa fa-map-marker"></i>Sector 134 Noida,IN</div>
+	<div class="follow"><b>Phone:</b><i class="fa fa-phone"></i>123456</div>
+	<div class="follow"><b>email:</b><i class="fa fa-envelope"></i>abc@gmail.com</div>
+		<div class="follow"><label><b>Get Social:</b></label>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-youtube-play"></i></a>
+				<a href="#"><i class="fa fa-whatsapp"></i></a>
+				<a href="#"><i class="fa fa-instagram"></i></a>
+				<a href="#"><i class="fa fa-google-plus"></i></a>
+			</div>
+		</div>
+	</div>
+	</div>
+
+</section>
+
+
+
+<section id="footer">
+
+	<div class="container text-center">
+		<p>Made With<i class="fa fa-heart-o"></i> By Siddhant Gupta</p>
+	</div>
+</section>
+</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
+<style>
+#home{
+	margin : 0;
+	padding : 0;
 }
-</script>
+/*----about----*/
+#about
+{
+	padding-top: 50px;
+	padding-bottom: 50px;
+	color:#555;
+}
+#about.btn
+{
+	margin-top: 20px;
+	margin-bottom: 30px;
+
+}
+.about-content
+{
+	padding-top: 20px;
+
+}
+.skill-bar p
+{
+	margin-bottom: 6px;
+	font-weight: 600;
+}
+.progree-bar
+{
+	border-radius:16px;
+}
+.progress
+{
+	border-radius: 16px !important;
+	margin-bottom: 20px;
+}
+#services
+{
+	background-image: linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8))W;
+	background-size: cover;
+	color:#efefef !important;
+	background-attachment: fixed;
+	padding-top: 50px;
+	padding-bottom : 50px;
+
+}
+#services h1
+{
+	text-align:center;
+	color:#efefef !important;
+	padding-bottom: 10px;
+}
+#services h1::after
+{
+	content:'';
+	background: #efefef;
+	display: block;
+	height: 3px; 
+	width:170px;
+	margin:20px auto 5px;
+}
+.services
+{
+	margin-top:40px;
+}
+.icon
+{
+	font-size: 40px;
+	margin:20px auto;
+	padding:20px;
+	height:80px;
+	width: 80px;
+	border: 1px solid #fff;
+	border-radius: 50%;
+}
+#services p
+{
+	font-size: 14px;
+	margin-top:20px;
+	color:#ccc;
+}
+.services .col-md-3 :hover
+{
+	background: #007bff;
+	cursor: pointer;
+	transition: 0.7s;
+}
+/*------*/
+#team
+{
+	padding-top:20px;
+	padding-bottom: 20px;
+	color:#555; 
+}
+h1
+{
+	text-align: center;
+	color:#555 !important;
+	padding-bottom: 10px;
+}
+
+h1::after
+{
+	content: '';
+	background: #007bff;
+	display: block;
+	height: 3px;
+	width: 170px;
+	margin: 20px auto 5px;
+}
+.profile-pic
+{
+	margin-top: 20px;
+}
+.profile-pic .img-box
+{
+	opacity: 1;
+	display: block;
+	position :relative;
+}
+.profile-pic .img-box img
+{
+	filter :grayscale(1);
+}
+.profile-pic .img-box img:hover
+{
+	filter :grayscale(0);
+	cursor:pointer;
+}
+.profile-pic h2
+{
+	font-size: 22px;
+	font-weight: bold;
+	margin-top: 15px;
+	color: #007bff !important;
+}
+.profile-pic h3
+{
+	font-size: 15px;
+	font-weight: bold;
+	margin-top: 15px;
+	
+}
+#team .fa
+{
+	height: 25px;
+	width: 25px;
+	color:#007bff !important;
+	background: #fff;
+	padding :4px;
+	border-radius: 50%;
+
+}
+	
+.img-box ul
+{
+	padding: 15px 0;
+	position: absolute;
+	z-index: 2;
+	bottom: 0;
+	left: 50%;
+	transform: translateX(-50%);
+	opacity: 0;
+
+
+}
+.img-box ul li
+{
+	padding: 5px;
+	display: inline-block;
+}
+.img-box:hover ul
+{
+	opacity: 1;
+}
+
+.img-box ul, .img-box ul li
+{
+	transition: 0.5s;
+}
+
+#promo
+{
+	background-image: linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7));
+	background-size: cover;
+	background-position: center;
+	color:#fff;
+	background-attachment: fixed;
+	text-align:center;
+	padding: 100px;
+	font-family: sans-serif;
+	font-size: 38px;
+
+	} 
+
+#price
+{
+ padding:20px 0;
+ background: #efefef;
+}
+.single-price
+{
+	margin: 10px auto;
+	display: inline;
+	background-color: #fff;
+	transition: 0.5s;
+
+}
+.single-price:hover
+{
+	box-shadow: 0 2px 20px #333;
+}
+.price-head
+{
+	background-color: #2196f3;
+	display: inline;
+	padding: 10px 5px;
+	text-align: center;
+}
+.price-head h2
+{
+	color: #fff;
+	font-size: 30px;
+	font-weight: bold;
+	margin-bottom: 5px;
+	padding: 5px;
+	text-transform: uppercase;
+}
+.price-head p
+{
+	font-size: 25px;
+	color:#ccc;
+	font-weight: bold;
+	line-height: 30px;
+
+}
+#price span
+{
+	font-size: 15px;
+}
+.price-content
+{
+	display: inline;
+	padding: 0 15px;
+
+}
+.price-content ul li
+{
+	border-bottom: 1px solid #efefef;
+	padding: 10px 10px;
+	list-style: none;
+
+}
+
+ul li:last-child
+{
+	border:none;
+}
+
+.fa-check-circle
+{
+	color:green;
+	margin-right: 10px;
+	font-size:20px;
+}
+.fa-times-circle
+{
+	color:red;
+	margin-right: 10px;
+	font-size:20px;
+}
+.price-button
+{
+	display: inline;
+	padding: 15px 15px 30px;
+	text-align: center;
+
+}
+.buy-btn
+{
+	background-color: #2196f3;
+	border-radius: 3px;
+	display: inline-block;
+	font-size: 18px;
+	padding: 15px 50px;
+	transition: 0.5s;
+
+}
+.buy-btn:hover{
+	border-color: 1px solid #fff;
+	background-color: #3f51b5;
+
+
+}
+a
+{
+	text-decoration: none!important;
+	color: #fff!important;
+
+}
+
+#testmonials
+{
+	padding-top: 50px;
+	padding-bottom: 50px;
+}
+#testmonials .row
+{
+	margin-top: 30px;
+}
+.col-md-4
+{
+	margin: 40px auto;
+}
+.profile
+{
+	padding: 70px 10px 10px;
+	background-color: #efefef;
+}
+.user{
+	width: 120px;
+	height: 120px ;
+	border-radius:50%;
+}
+.profile img
+{
+	top:-60px;
+	position: absolute;
+	left: calc(50% - 60px);
+	border: 10px solid #fff;
+}
+.profile h3
+{
+	font-size: 20px;
+	margin-top: 15px;
+	color: #007bff;
+
+}
+#testmonials span
+{
+	font-size: 12px;
+	color: #333;
+}
+blockquote
+{
+	font-size:16px;
+	line-height: 30px;
+}
+blockquote::before
+{
+	content: '\93';
+	font-size: 50px;
+	color: #007bff;
+	position: relative;
+	line-height: 20px;
+	bottom: -15px;
+	right: 5px;
+}
+blockquote::after
+{
+	content: '\94';
+	font-size: 50px;
+	color: #007bff;
+	position: relative;
+	line-height: 10px;
+	bottom: -15px;
+	left: 5px;
+}
+.profile:hover
+{
+	box-shadow: 0 0 15px 5px rgba(0,0,0,0.2);
+	cursor: pointer;
+	transition: 0.5s;
+}
+
+#contact
+{
+	background-color: #efefef;
+	padding-top: 40px;
+	padding-bottom: 40px;
+	color: #777;
+
+}
+.contact-form
+{
+	padding: 15px;
+
+}
+.form-control
+{
+	border-radius: 0 !important;
+	border:none !important;
+}
+::placeholder
+{
+	color: #999 !important;
+}
+.follow
+{
+	background:#fff;
+	padding: 10px;
+	margin: 15px;
+
+}
+.contact-info .fa
+{
+	margin: 10px;
+	color:#007bff;
+	font-weight: bold;
+
+}
+#footer{
+	background:#333;
+	color: #fff;
+	padding: 12px;
+}
+.fa-heart-o
+{
+	margin: 3px;
+	color: red;
+}
+</style>
