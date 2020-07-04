11564dfc29045bb68f89384b9f2c027de7d672d4
sonali <sona120920@gmail.com>
2020-07-04 16:37:12 +0530

    Filed added

---
+++ src/components/team.vue
@@ -0,0 +1,82 @@
+<template>
+<div class="container">
+	<h1>OUR TEAM</h1>
+	<div class="row">
+		<div class="col-md-3 profile-pic text-center">
+				<div class="img-box">
+					<img src="me.jpg" class="img-responsive">
+					<ul>
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-whatsapp"></i></li></a>
+					<a href="#"><li><i class="fa fa-instagram"></i></li></a>
+					</ul>
+				</div>
+				<h2>Siddhant Gupta</h2>
+				<h3>Founder/CEO</h3>
+				<p>Like and ask question in comment.
+				</p>
+			</div>
+				<div class="col-md-3 profile-pic text-center">
+				<div class="img-box">
+					<img src="IMG-20190127-WA0028.jpg" class="img-responsive">
+					<ul>
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-whatsapp"></i></li></a>
+					<a href="#"><li><i class="fa fa-instagram"></i></li></a>
+					</ul>
+				</div>
+				<h2>Sonali</h2>
+				<h3>Business HEAD</h3>
+				<p>Like and ask question in comment.
+				</p>
+			</div>
+				<div class="col-md-3 profile-pic text-center">
+				<div class="img-box">
+					<img src="IMG-20200222-WA0013.jpg" class="img-responsive">
+					<ul>
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-whatsapp"></i></li></a>
+					<a href="#"><li><i class="fa fa-instagram"></i></li></a>
+					</ul>
+				</div>
+				<h2>Harshit Garg</h2>
+				<h3>Marketing head</h3>
+				<p>Like and ask question in comment.
+				</p>
+			</div>
+				<div class="col-md-3 profile-pic text-center">
+				<div class="img-box">
+					<img src="IMG-20191008-WA0020.jpg" class="img-responsive">
+					<ul>
+					<a href="#"><li><i class="fa fa-twitter"></i></li></a>
+					<a href="#"><li><i class="fa fa-linkedIn"></i></li></a>
+					<a href="#"><li><i class="fa fa-instagram"></i></li></a>
+					</ul>
+				    </div>
+				         <h2>Sangeeta Jha</h2>
+				         <h3>UI/UX  Designer</h3>
+				         <p>lLke and ask question in comment.
+				         </p>
+			</div>
+		    </div>
+	</div>
+</section>
+
+
+
+
+<section id="promo">
+<div class="container">
+	<p>Get free passes and Goodies and exciting hampers</p>
+	<a href="#" class="btn btn->primary">CONTACT US</a>
+</div>
+</section>
+
+
+
+
+
+
+
+
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,17 +9,37 @@
         <img src="https://image.freepik.com/free-vector/business-leader-consulting-hr-expert_1262-21207.jpg" alt="">
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-12">
+      <Services/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+      <Team/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+      <Footer/>
+      </div>
+    </div>
+    </div>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Footer from "@/components/common/footer";
+import Team from "@/components/team";
+import Services from "@/components/services";
 
 export default {
   name: 'Home',
   components: {
-    
+    Footer,
+    Services,
+    Team
   }
 }
-</script>
